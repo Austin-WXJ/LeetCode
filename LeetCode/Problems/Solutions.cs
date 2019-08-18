ccba908ce6297c5d080110e9ef620df803af31e3
Austin <austin.wxj@gmail.com>
2019-08-18 18:07:56 -0400

    send to github

---
+++ LeetCode/Problems/Solutions.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problems {
+    public class Solutions {
+        public int[] TwoSum_Brute(int[] nums, int target)
+        {
+            int[] result = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int n = 0; n < nums.Length; n++)
+                {
+                    if (i != n && (target == (nums[i] + nums[n])))
+                    {
+                        result[0] = n;
+                        result[1] = i;
+                    }
+                }
+            }
+            return result;
+        }
+        public int[] TwoSum_SingleHash(int[] nums, int target)
+        {
+            SortedList<int, int> kvp = new SortedList<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int compliment = target - nums[i];
+
+                if (kvp.ContainsKey(compliment))
+                {
+                    return new int[2] { kvp[compliment], i };
+                }
+                else if (!kvp.ContainsKey(nums[i]))
+                {
+                    kvp.Add(nums[i], i);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            throw new Exception();
+        }
+
+        public int MyAtoi(string str)
+        {
+            var chunk = str.TrimStart().Split(" ")[0];
+            var allowedChars = "+-.";
+            string lastTry = string.Empty;
+
+            foreach (char item in chunk)
+            {
+                if (chunk.IndexOf(item) == 0 && allowedChars.Contains(item))
+                {
+                    lastTry += item;
+                }
+                else if (char.IsDigit(item))
+                {
+                    lastTry += item;
+                }
+                else if (chunk.IndexOf(item) > 0 && !char.IsDigit(item))
+                {
+                    break;
+                }
+            }
+
+            return ParseMyAtoi(lastTry);
+        }
+
+        private int ParseMyAtoi(string str)
+        {
+            bool isIntParsed = int.TryParse(str, out int result);
+            if (isIntParsed)
+            {
+                return result;
+            }
+
+            double.TryParse(str, out double thing);
+            bool isLong = long.TryParse(thing.ToString(), out long thing2);
+
+            if (isLong)
+            {
+                switch (Math.Sign(thing2))
+                {
+                    case 0:
+                        return 0;
+                    case -1:
+                        return int.MinValue;
+                    case 1:
+                        return int.MaxValue;
+                }
+            }
+
+            return 0;
+        }
+
+        public int Reverse(int x)
+        {
+            int result = 0;
+            if (x.GetType() == typeof(int))
+            {
+                int sign = Math.Sign(x);
+                if (x == int.MinValue)
+                {
+                    x = int.MinValue;
+                }
+                else
+                {
+                    x = Math.Abs(x);
+                }
+                char[] xString = x.ToString().ToCharArray();
+                string xReverse = new string(xString.Reverse().ToArray());
+                bool isStillInt = int.TryParse(xReverse, out result);
+                if (isStillInt)
+                {
+                    result = int.Parse(xReverse) * sign;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ LeetCode/Tests/Tests.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using Problems;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        private Solutions solution;
+
+        [SetUp]
+        public void Init()
+        {
+            solution = new Solutions();
+        }
+
+        [TestCase(new int[] { 3,3 }, new int[] { 0, 1 }, 6)]
+        [TestCase(new int[] { 1, 3, 4, 5 }, new int[] { 2, 3 }, 9)]
+        [TestCase(new int[] { 1, 3, 4, 5 }, new int[] { 0, 3 }, 6)]
+        [TestCase(new int[] { 9, 0 , 8, 3 }, new int[] { 1, 3 }, 3)]
+        [TestCase(new int[] { 9, 0, 8, 3 }, new int[] { 0, 2 }, 17)]
+        [TestCase(new int[] { 9, 0, 8, 3 }, new int[] { 0, 2 }, 17)]
+        public void TwoSum_Brute(int[] args, int[] target, int sum)
+        {
+            Assert.AreEqual(target, solution.TwoSum_Brute(args, sum));
+        }
+
+        [TestCase(new int[] { 3, 3 }, new int[] { 0, 1 }, 6)]
+        [TestCase(new int[] { 1, 3, 4, 5 }, new int[] { 2, 3 }, 9)]
+        [TestCase(new int[] { 1, 3, 4, 5 }, new int[] { 0, 3 }, 6)]
+        [TestCase(new int[] { 9, 0, 8, 3 }, new int[] { 1, 3 }, 3)]
+        [TestCase(new int[] { 9, 0, 8, 3 }, new int[] { 0, 2 }, 17)]
+        [TestCase(new int[] { 9, 0, 8, 3 }, new int[] { 0, 2 }, 17)]
+        public void TwoSum_SingleHash(int[] args, int[] target, int sum)
+        {
+            Assert.AreEqual(target, solution.TwoSum_SingleHash(args, sum));
+        }
+
+        [TestCase("", 0)]
+        [TestCase("+1", 1)]
+        [TestCase(".1", 0)]
+        [TestCase("+-2", 0)]
+        [TestCase("   -42", -42)]
+        [TestCase("words and 987", 0)]
+        [TestCase("4193 with words", 4193)] 
+        [TestCase("56456andNoWords",56456)]
+        [TestCase("-91283472332", int.MinValue)]
+        [TestCase("20000000000000000000", 2147483647)]
+        [TestCase("9223372036854775808", int.MaxValue)]
+        [TestCase("-91283472332andNoWords", int.MinValue)]
+        public void MyAtoi(string arg, int outcome)
+        {
+            Assert.AreEqual(outcome, solution.MyAtoi(arg));
+        }
+
+
+        [TestCase(123, 321)]
+        [TestCase(-123, -321)]
+        [TestCase(120, 21)]
+        [TestCase(1534236469, 0)]
+        [TestCase(int.MinValue, 0)]
+        public void Reverse(int arg, int result)
+        {
+            Assert.AreEqual(result, solution.Reverse(arg));
+        }
+    }
+}
